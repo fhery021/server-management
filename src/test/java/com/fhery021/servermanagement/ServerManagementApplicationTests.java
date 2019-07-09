b03bdf03ebe8f5cb306c7db41bb7b79584235406
Ferenc Bondar <fhery021@gmail.com>
2019-07-10 01:25:09 +0300

    init 2

---
+++ src/test/java/com/fhery021/servermanagement/ServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhery021.servermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
