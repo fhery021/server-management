af91373e2cb00bf1028aa18cbf0bf9a25c8cd44a
Ferenc Bondar <fhery021@gmail.com>
2019-07-19 10:15:55 +0300

    adding angular project

--- src/main/java/com/fhery021/servermanagement/controllers/ServerController.java
+++ src/main/java/com/fhery021/servermanagement/controllers/ServerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @Api(description = "Server management API")
 @RestController
 @RequestMapping(ServerController.BASE_URL)
-// TODO Test w. Angular -> @CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ServerController {
 
     static final String BASE_URL = "/api/servers";
